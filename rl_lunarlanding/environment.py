837920bf35e4a18bcd273ad28bf6a29a61cdbae8
Kevin BOULLIAT <k.boulliat@gmail.com>
2023-06-06 17:47:42 +0200

    Class Planet done 18h

--- rl_lunarlanding/environment.py
+++ rl_lunarlanding/environment.py
@@ -2,65 +2,61 @@ import gym
 import pandas as pd
 
 
-def get_env(render_mode = "human"):
-    """
-    Returns a gym environment. Replace by a custom environment if needed.
-    """
-
-    # We use the LunarLander env. Other environments are available.
-    return gym.make("LunarLander-v2", render_mode)
+#Terre , Lune , Mars, Jupiter
 
 
+# Importer la classe
+import gym
 
-def run_env(env, agt, run_number):
-    """
-    Run a given environment with a given agent.
-    """
+class Planet:
+    def __init__(self, render_mode="human"):
+        self.render_mode = render_mode
 
-    #Initialisation du fichier pickle
-    fichier = "data_saved.pickle"
-    fichier_pickle = open(fichier,'wb')
+    def select_planet(self, planet):
+        if planet == "Terre":
+            return gym.make("LunarLander-v2",
+                            render_mode=self.render_mode,
+                            gravity=-8,
+                            enable_wind=True,
+                            wind_power=10)
 
-    #obs_old, info = env.reset(seed=CFG.rnd_seed)
-    obs_old, info = env.reset()
+        if planet == "Lune":
+            return gym.make("LunarLander-v2",
+                            render_mode=self.render_mode,
+                            gravity=-1,
+                            enable_wind=False)
 
-    # We get the action space.
-    act_space = env.action_space
+        if planet == "Mars":
+            return gym.make("LunarLander-v2",
+                            render_mode=self.render_mode,
+                            gravity=-3,
+                            enable_wind=True,
+                            wind_power=4)
 
-    frame = 0
-    score = 0
+        if planet == "Jupiter":
+            return gym.make("LunarLander-v2",
+                            render_mode=self.render_mode,
+                            gravity=-11.5)
 
-    print(f"Run number: {run_number + 1}")
+        if planet == "Terre_vent":
+            return gym.make("LunarLander-v2",
+                            render_mode=self.render_mode,
+                            gravity=-8,
+                            enable_wind=True,
+                            wind_power=18)
 
-    for _ in range(1000):
+        else:
+            print("La planète sélectionnée n'est pas disponible")
 
-        # We can visually render the learning environment. We disable it for performance.
-        env.render()
 
-        # We request an action from the agent.
-        act = agt.get(obs_old, act_space)
 
-        # We apply the action on the environment.
-        obs_new, rwd, terminated, truncated, _ = env.step(act)
+def get_metrics():
+    #We update our information
+    frame += 1
+    score += rwd
 
-        # We perform a learning step.
-        agt.set(obs_old, act, rwd, obs_new)
-
-        # We store the data
-        data = (obs_new , obs_old , rwd, terminated)
-
-        # Update latest observation
-        obs_old = obs_new
-
-        # We add the needed data to our file :
-        fichier_pickle.dump(data , fichier_pickle)
-
-        #We update our information
-        frame += 1
-        score += rwd
-
-        if terminated or truncated:
-            obs_end, info = env.reset()
+        #if terminated or truncated:
+            #obs_end, info = env.reset()
 
     # We define the macro data
     agent_type = agt
@@ -71,8 +67,3 @@ def run_env(env, agt, run_number):
     # We add the macro data to a Df
     data_for_df = [agent_type,game_over,time,score_reward]
     df = pd.DataFrame(data_for_df,columns=['Agent','Game Over','Time','Score'])
-
-
-    env.close()
-
-    return df , fichier_pickle
